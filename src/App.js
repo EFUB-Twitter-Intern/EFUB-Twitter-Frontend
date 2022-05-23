2361d869a80a1323015b9292861867dd9963f71a
sohyundoh <lyny123@naver.com>
2022-05-23 20:14:20 +0900

    dsh/image 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import Menu from "components/Menu";
-
+import './App.css';
+import Follow from 'components/Follow';
+import Trend from 'components/Trend';
+import Search from 'components/Search';
+import Twitts from 'components/Twitts';
+import React from 'react';
 function App() {
-  return <Menu></Menu>;
-}
+  return(
+    <div>
+      <Search></Search>
+      <Trend></Trend>
+      <Follow></Follow>
+    </div>
+  )
+};
 
 export default App;

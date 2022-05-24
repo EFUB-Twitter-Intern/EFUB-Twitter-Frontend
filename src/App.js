d606ec2e1acf9a8c65bff150a8b04ce0b5d2442a
sohyundoh <lyny123@naver.com>
2022-05-24 15:59:51 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import './App.css';
 import Follow from 'components/Follow';
 import Trend from 'components/Trend';
 import Search from 'components/Search';
-import Twitts from 'components/Twitts';
 import React from 'react';
+import styled from 'styled-components';
+
+const Right_sidebar = styled.div`
+    align-items: center;
+    flex-direction: column;
+    width: 25%;
+`;
+const Wrapper =styled.div`
+  flex-direction: row;
+`;
+
 function App() {
   return(
-    <div>
-      <Search></Search>
-      <Trend></Trend>
-      <Follow></Follow>
-    </div>
+    <Wrapper>
+      <Right_sidebar>
+        <Search></Search>
+        <Trend></Trend>
+        <Follow></Follow>
+      </Right_sidebar>
+    </Wrapper>
   )
 };
 
